534347f194f63f8d83569e1541df2d3a40710bd2
marcelosrg <marcelohrs2005@gmail.com>
2026-09-09 14:53:59 -0300

    chore: initial commit

---
+++ src/main/java/io/github/marcelosrg/backetservice/BacketserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.marcelosrg.backetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacketserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacketserviceApplication.class, args);
+	}
+
+}
